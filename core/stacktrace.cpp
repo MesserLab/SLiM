54b3b727af2c20db6a3c263125ba2ca3c4d14da7
Ben Haller <github@sticksoftware.com>
2014-12-26 23:33:51 -0500

    Exclude a built executable of slim from git

---
+++ core/stacktrace.cpp
@@ -0,0 +1,9 @@
+//
+//  stacktrace.cpp
+//  SLiM
+//
+//  Created by Ben Haller on 12/26/14.
+//  Copyright (c) 2014 Messer Lab, http://messerlab.org/software/. All rights reserved.
+//
+
+#include <stdio.h>
