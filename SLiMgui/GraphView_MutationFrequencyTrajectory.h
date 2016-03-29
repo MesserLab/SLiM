54a16f565cfbc9f2acbc6c4a4c9e523a9dc54880
Ben Haller <github@sticksoftware.com>
2016-03-28 20:00:22 -0400

    Add missing copyright notices

--- SLiMgui/GraphView_MutationFrequencyTrajectory.h
+++ SLiMgui/GraphView_MutationFrequencyTrajectory.h
@@ -3,6 +3,7 @@
 //  SLiM
 //
 //  Created by Ben Haller on 3/11/15.
+//  Copyright (c) 2015-2016 Philipp Messer.  All rights reserved.
 //	A product of the Messer Lab, http://messerlab.org/slim/
 //
 
