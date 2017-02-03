2d147ff760f4961ec831336f5c5f909a72ccdd36
Ben Haller <github@sticksoftware.com>
2017-02-03 11:09:29 +0900

    Advance version to 2.1.1 candidate

--- eidos/eidos_functions.cpp
+++ eidos/eidos_functions.cpp
@@ -6941,7 +6941,7 @@ EidosValue_SP Eidos_ExecuteFunction_version(__attribute__((unused)) const EidosV
 	
 	std::ostringstream &output_stream = p_interpreter.ExecutionOutputStream();
 	
-	output_stream << "Eidos version 1.2" << endl;	// EIDOS VERSION
+	output_stream << "Eidos version 1.2.1" << endl;	// EIDOS VERSION
 	
 	if (gEidosContextVersion.length())
 		output_stream << gEidosContextVersion << endl;
