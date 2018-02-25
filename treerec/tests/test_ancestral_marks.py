cbab957980c8a0489b3507b2c77db246840fb723
peter <petrel.harp@gmail.com>
2018-02-24 20:15:54 -0800

    testing script

--- treerec/tests/test_ancestral_marks.py
+++ treerec/tests/test_ancestral_marks.py
@@ -4,19 +4,17 @@ def check_consistency(x, y):
     # here y is an iterable of disjoint lists of ids (determined by indivs who share mutations in SLiM)
     # and x is a dict whose keys are ids and whose values are labels (the roots in msprime);
     # check that any two ids in the same element of y have the same label in x
-    print("x:", x)
-    print("y:", y)
     all_ids = []
     for a in y:
         if len(y[a]) > 0:
             a_labels = []
             for u in y[a]:
-                print("u:", u)
                 assert(u in x)
                 assert(u not in all_ids)
                 all_ids.append(u)
                 a_labels.append(x[u])
-            print('al:', a_labels)
+            print('IDs sharing mutation', a, ":", y[a])
+            print('labels of roots in from tree:', a_labels)
             assert(len(set(a_labels)) == 1)
 
 
@@ -25,12 +23,13 @@ node_file = open("NodeTable.txt", "r")
 edge_file = open("EdgeTable.txt", "r")
 ts = msprime.load_text(nodes=node_file, edges=edge_file, decode_metadata=False)
 
-# get msprime -> SLiM ID map from metadata
+# get SLiM ID -> msprime ID map from metadata
 ids = {}
 for n in ts.nodes():
     meta = n.metadata.decode('utf8')
     assert(meta[:7] == "SLiMID=")
-    ids[n.id] = int(n.metadata.decode('utf8').strip().split("=")[1])
+    slim_id = int(n.metadata.decode('utf8').strip().split("=")[1])
+    ids[slim_id] = n.id
 
 # iterate through SLiM output information
 slim_file = open("TESToutput.txt", "r")
@@ -46,20 +45,23 @@ for header in slim_file:
     pos = int(pos)
     if pos == len(slim):
         slim.append({})
-    slim[pos][mut] = [int(u) for u in slim_file.readline().split()]
+    slim[pos][mut] = [ids[int(u)] for u in slim_file.readline().split()]
 
 
 # test these agree
 pos = 0
 for t in ts.trees():
     # get partition of leaves from this tree, using SLiM IDs
+    print(t.draw(format="unicode",height = 200))
+    print("left:", t.interval[0], "right:", t.interval[1])
     fams = {}
-    for x in t.leaves():
+    for x in t.nodes():
         u = x
         while t.parent(u) != msprime.NULL_NODE:
             u = t.parent(u)
-        fams[ids[x]] = u
+        fams[x] = u
     while pos < t.interval[1]:
+        print("pos:", pos, "------------")
         assert(check_consistency(fams, slim[pos]))
         pos += 1
 
