88a15d6a1f5556d34402d023bed0d2ef7beac94d
Ben Haller <github@sticksoftware.com>
2019-01-29 18:02:12 -0500

    increment version to 3.2.1 (Eidos 2.2.1)

--- eidos/eidos_global.h
+++ eidos/eidos_global.h
@@ -39,8 +39,8 @@ class EidosScript;
 class EidosToken;
 
 
-#define EIDOS_VERSION_STRING	("2.2")
-#define EIDOS_VERSION_FLOAT		(2.2)
+#define EIDOS_VERSION_STRING	("2.2.1")
+#define EIDOS_VERSION_FLOAT		(2.21)
 
 
 // This should be called once at startup to give Eidos an opportunity to initialize static state
