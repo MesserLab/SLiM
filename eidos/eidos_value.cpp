fe5cc492320215623c548ae049e822c9435c12d7
Ben Haller <github@sticksoftware.com>
2019-05-01 17:02:37 -0400

    fix leak and unnecessary mallocs in EidosValue::Subset()

--- eidos/eidos_value.cpp
+++ eidos/eidos_value.cpp
@@ -569,7 +569,15 @@ EidosValue_SP EidosValue::Subset(std::vector<std::vector<int64_t>> &p_inclusion_
 		// Finally, set the dimensionality of the result, considering dropped dimensions.  This basically follows the structure
 		// of the indexed operand's dimensions, but (a) resizes to match the size of p_inclusion_indices for the given dimension,
 		// and (b) omits any dimension that has a count of exactly 1, if dropping is requested.
-		int64_t *dim = (int64_t *)malloc(dimcount * sizeof(int64_t));
+		static int64_t *static_dim_buffer = nullptr;
+		static int static_dim_buffer_size = -1;
+		
+		if (dimcount > static_dim_buffer_size)
+		{
+			static_dim_buffer = (int64_t *)realloc(static_dim_buffer, (dimcount + 1) * sizeof(int64_t));	// +1 so the zero case doesn't result in a zero-size allocation
+			static_dim_buffer_size = dimcount;
+		}
+		
 		int final_dim_count = 0;
 		
 		for (int subset_index = 0; subset_index < dimcount; ++subset_index)
@@ -577,11 +585,11 @@ EidosValue_SP EidosValue::Subset(std::vector<std::vector<int64_t>> &p_inclusion_
 			int dim_size = inclusion_counts[subset_index];
 			
 			if (!p_drop || (dim_size > 1))
-				dim[final_dim_count++] = dim_size;
+				static_dim_buffer[final_dim_count++] = dim_size;
 		}
 		
 		if (final_dim_count > 1)
-			result_SP->SetDimensions(final_dim_count, dim);
+			result_SP->SetDimensions(final_dim_count, static_dim_buffer);
 	}
 	
 	return result_SP;
