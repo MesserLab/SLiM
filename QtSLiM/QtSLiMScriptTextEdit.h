b5c0425e20fcb6ffb437afe9081ad812dda8c8af
Ben Haller <github@sticksoftware.com>
2019-12-07 12:56:54 -0500

    add copyright notices and license blurbs

--- QtSLiM/QtSLiMScriptTextEdit.h
+++ QtSLiM/QtSLiMScriptTextEdit.h
@@ -1,3 +1,22 @@
+//
+//  QtSLiMScriptTextEdit.h
+//  SLiM
+//
+//  Created by Ben Haller on 11/24/2019.
+//  Copyright (c) 2019 Philipp Messer.  All rights reserved.
+//	A product of the Messer Lab, http://messerlab.org/slim/
+//
+
+//	This file is part of SLiM.
+//
+//	SLiM is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//	SLiM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License along with SLiM.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef QTSLIMSCRIPTTEXTEDIT_H
 #define QTSLIMSCRIPTTEXTEDIT_H
 
