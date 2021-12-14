69f46f87fdf2480b0f873e6d152b61eb921443c6
Ben Haller <github@sticksoftware.com>
2021-12-13 20:55:11 -0500

    increment version number for release

--- core/slim_globals.h
+++ core/slim_globals.h
@@ -41,8 +41,8 @@ class Subpopulation;
 class SLiMEidosBlock;
 
 
-#define SLIM_VERSION_STRING	("3.6")
-#define SLIM_VERSION_FLOAT	(3.6)
+#define SLIM_VERSION_STRING	("3.7")
+#define SLIM_VERSION_FLOAT	(3.7)
 
 
 // This should be called once at startup to give SLiM an opportunity to initialize static state
