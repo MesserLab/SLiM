aa6b0a697732468a21a21767307bb079f058636b
Ben Haller <github@sticksoftware.com>
2023-04-13 10:41:56 -0400

    remove old dark/light mode objc cruft

--- QtSLiM/QtSLiM.pro
+++ QtSLiM/QtSLiM.pro
@@ -143,9 +143,6 @@ else:win32:!win32-g++:CONFIG(release, debug|release): PRE_TARGETDEPS += $$OUT_PW
 else:win32:!win32-g++:CONFIG(debug, debug|release): PRE_TARGETDEPS += $$OUT_PWD/../eidos_zlib/debug/eidos_zlib.lib
 else:unix: PRE_TARGETDEPS += $$OUT_PWD/../eidos_zlib/libeidos_zlib.a
 
-# Objective-C runtime library dependency (macOS only), for forcing light mode in main.cpp (see macos_ForceLightMode())
-macx: LIBS += -lobjc.A
-
 
 SOURCES += \
     ../cmake/GitSHA1_qmake.cpp \
--- QtSLiM/main.cpp
+++ QtSLiM/main.cpp
@@ -12,15 +12,6 @@
 #include <locale.h>
 
 #include "eidos_globals.h"
-#include "eidos_symbol_table.h"
-
-// Include objc headers if needed for macos_ForceLightMode()
-#ifdef __APPLE__
-#if (QT_VERSION < QT_VERSION_CHECK(5, 15, 2))
-#include <objc/runtime.h>
-#include <objc/message.h>
-#endif
-#endif
 
 #if SLIM_LEAK_CHECKING
 static void clean_up_leak_false_positives(void)
@@ -57,8 +48,16 @@ static void clean_up_leak_false_positives(void)
 // removed.  As per the above comment, this may mean that macOS builds against a Qt version
 // earlier than 5.15.2 will not succeed in forcing light mode.  For this reason and others,
 // we now require Qt 5.15.2 when building on macOS (see QtSLiMAppDelegate.cpp).
+// BCH 4/13/2022: Disabling this macos_ForceLightMode() function altogether.  Building against
+// Qt versions prior to 5.15.2 on macOS is no longer supported at all, and linking against
+// libobjc.dylib on macOS 12 has gotten complicated for security reasons (trampolines).
 #ifdef __APPLE__
 #if (QT_VERSION < QT_VERSION_CHECK(5, 15, 2))
+#if 0
+// Include objc headers
+#include <objc/runtime.h>
+#include <objc/message.h>
+
 static void macos_ForceLightMode(void)
 {
     // First we need to make an NSString with value @"NSAppearanceNameAqua"; 1 is NSASCIIStringEncoding
@@ -131,6 +130,7 @@ static void macos_ForceLightMode(void)
     if (class_respondsToSelector(nsapp_class, selector_setAppearance))
         ((void (*)(id, SEL, id))objc_msgSend)(sharedApplication, selector_setAppearance, aquaAppearance);
 }
+#endif
 #endif
 #endif
 
@@ -251,7 +251,8 @@ int main(int argc, char *argv[])
     // mean that most macOS users get dark mode support.
 #ifdef __APPLE__
 #if (QT_VERSION < QT_VERSION_CHECK(5, 15, 2))
-    macos_ForceLightMode();
+#warning Building on macOS with a Qt version less than 5.15.2; dark/light mode may not work correctly
+    //macos_ForceLightMode();
 #endif
 #endif
     
