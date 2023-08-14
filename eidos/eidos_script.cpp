5a69fb103347d42c0729a8084046a959ed90af7b
Ben Haller <github@sticksoftware.com>
2023-08-14 09:50:18 -0400

    get rid of bidi-chars warnings

--- eidos/eidos_script.cpp
+++ eidos/eidos_script.cpp
@@ -694,6 +694,11 @@ void EidosScript::Tokenize(bool p_make_bad_tokens, bool p_keep_nonsignificant)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunknown-warning-option"
 #pragma clang diagnostic ignored "-Wbidi-chars"
+				// BCH 8/14/2023: Note that seven lines are commented out below.  They are correct, but they
+				// produce a warning "unpaired UTF-8 bidirectional control character detected [-Wbidi-chars=]",
+				// and disabling the bidi-chars warning does not suppress that warning, annoyingly.  This is
+				// very edge anyhow, so I'm commenting these lines out for now.  A better fix should be
+				// possible, but it doesn't seem worth worrying about for now.
 				contains_illegal = contains_illegal || (identifierString.find("\u2000") != std::string::npos);
 				contains_illegal = contains_illegal || (identifierString.find("\u2001") != std::string::npos);
 				contains_illegal = contains_illegal || (identifierString.find("\u2002") != std::string::npos);
@@ -712,11 +717,11 @@ void EidosScript::Tokenize(bool p_make_bad_tokens, bool p_keep_nonsignificant)
 				contains_illegal = contains_illegal || (identifierString.find("\u200f") != std::string::npos);
 				contains_illegal = contains_illegal || (identifierString.find("\u2028") != std::string::npos);
 				contains_illegal = contains_illegal || (identifierString.find("\u2029") != std::string::npos);
-				contains_illegal = contains_illegal || (identifierString.find("\u202a") != std::string::npos);
-				contains_illegal = contains_illegal || (identifierString.find("\u202b") != std::string::npos);
+				//contains_illegal = contains_illegal || (identifierString.find("\u202a") != std::string::npos);
+				//contains_illegal = contains_illegal || (identifierString.find("\u202b") != std::string::npos);
 				contains_illegal = contains_illegal || (identifierString.find("\u202c") != std::string::npos);
-				contains_illegal = contains_illegal || (identifierString.find("\u202d") != std::string::npos);
-				contains_illegal = contains_illegal || (identifierString.find("\u202e") != std::string::npos);
+				//contains_illegal = contains_illegal || (identifierString.find("\u202d") != std::string::npos);
+				//contains_illegal = contains_illegal || (identifierString.find("\u202e") != std::string::npos);
 				contains_illegal = contains_illegal || (identifierString.find("\u202f") != std::string::npos);
 				contains_illegal = contains_illegal || (identifierString.find("\u205f") != std::string::npos);
 				contains_illegal = contains_illegal || (identifierString.find("\u2060") != std::string::npos);
@@ -725,9 +730,9 @@ void EidosScript::Tokenize(bool p_make_bad_tokens, bool p_keep_nonsignificant)
 				contains_illegal = contains_illegal || (identifierString.find("\u2063") != std::string::npos);
 				contains_illegal = contains_illegal || (identifierString.find("\u2064") != std::string::npos);
 				contains_illegal = contains_illegal || (identifierString.find("\u2065") != std::string::npos);
-				contains_illegal = contains_illegal || (identifierString.find("\u2066") != std::string::npos);
-				contains_illegal = contains_illegal || (identifierString.find("\u2067") != std::string::npos);
-				contains_illegal = contains_illegal || (identifierString.find("\u2068") != std::string::npos);
+				//contains_illegal = contains_illegal || (identifierString.find("\u2066") != std::string::npos);
+				//contains_illegal = contains_illegal || (identifierString.find("\u2067") != std::string::npos);
+				//contains_illegal = contains_illegal || (identifierString.find("\u2068") != std::string::npos);
 				contains_illegal = contains_illegal || (identifierString.find("\u2069") != std::string::npos);
 				contains_illegal = contains_illegal || (identifierString.find("\u206a") != std::string::npos);
 				contains_illegal = contains_illegal || (identifierString.find("\u206b") != std::string::npos);
