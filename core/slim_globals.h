c4c00a67b8c6dfcf4f7e0d7e13a27889e27e8903
nobrien97 <nicholasobrien97@gmail.com>
2023-10-10 11:47:40 +1000

    Changed laplace DFE string to p

--- core/slim_globals.h
+++ core/slim_globals.h
@@ -1077,7 +1077,7 @@ extern const std::string &gStr_g;
 extern const std::string &gStr_e;
 //extern const std::string &gStr_n;		now gEidosStr_n
 extern const std::string &gStr_w;
-extern const std::string &gStr_la;
+extern const std::string &gStr_p;
 extern const std::string &gStr_l;
 //extern const std::string &gStr_s;		now gEidosStr_s
 extern const std::string &gStr_species;
@@ -1471,7 +1471,7 @@ enum _SLiMGlobalStringID : int {
 	gID_e,
 	// gID_n,		now gEidosID_n
 	gID_w,
-	gID_la,
+	gID_p,
 	gID_l,
 	//gID_s,	now gEidosID_s
 	gID_species,
