dc3c4a319b265129abc6d32cf6a5b2690d8f5ef7
Andrew Kern <adkern@uoregon.edu>
2025-12-17 15:38:04 -0800

    update benchmark script

--- core/spatial_map.cpp
+++ core/spatial_map.cpp
@@ -1652,7 +1652,7 @@ EidosValue_SP SpatialMap::ExecuteMethod_power(EidosGlobalStringID p_method_id, c
 	if ((x_value->Type() == EidosValueType::kValueInt) || (x_value->Type() == EidosValueType::kValueFloat))
 	{
 		double power_scalar = x_value->NumericAtIndex_NOCAST(0, nullptr);
-
+		
 		// FIXME: TO BE PARALLELIZED
 		Eidos_SIMD::pow_float64_scalar_exp(values_, power_scalar, values_, values_size_);
 	}
@@ -1660,10 +1660,10 @@ EidosValue_SP SpatialMap::ExecuteMethod_power(EidosGlobalStringID p_method_id, c
 	{
 		SpatialMap *power_map = (SpatialMap *)x_value->ObjectElementAtIndex_NOCAST(0, nullptr);
 		double *power_map_values = power_map->values_;
-
+		
 		if (!IsCompatibleWithMap(power_map))
 			EIDOS_TERMINATION << "ERROR (SpatialMap::ExecuteMethod_power): power() requires the target SpatialMap to be compatible with the SpatialMap supplied in x (using the same spatiality and bounds, and having the same grid resolution)." << EidosTerminate();
-
+		
 		// FIXME: TO BE PARALLELIZED
 		Eidos_SIMD::pow_float64(values_, power_map_values, values_, values_size_);
 	}
